32dedd5a56ba3ad5545212aadc0cb4bf9315d542
Huzaifa Ghouri <120574411+HuzaifaGhouri08@users.noreply.github.com>
2025-01-31 00:32:02 +0500

    Updated Python Skill

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const skills = [
   { name: "HTML", percentage: 94 },
   { name: "CSS", percentage: 90 },
-  { name: "JavaScript / TypeScript", percentage: 85 },
+  { name: "JavaScript", percentage: 85 },
   { name: "Next.js", percentage: 75 },
   { name: "Tailwind.css", percentage: 70 },
   { name: "Python", percentage: 30 },
